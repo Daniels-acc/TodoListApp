782d74d1a7abb1eb1957920568a32368d7ca5220
daniel.gluhak <gluhakdaniel@gmail.com>
2023-05-06 08:56:29 +0200

    Introducing functions. Getting rid of repetitive code.

--- main.py
+++ main.py
@@ -8,41 +8,50 @@ file_list = os.listdir(folder_path_src)
 # --------------------------------------
 
 # --------------------------------------
+# functions:
+def get_list():
+    with open('todo_list.txt', 'r') as file_local:
+        todo_list_local = file_local.readlines()
+    return todo_list_local
+
+def set_list():
+    with open('todo_list.txt', 'w') as file:
+        file.writelines(todo_list)
+    return todo_list
+
+# --------------------------------------
 
 while True:
+
     user_input = input('Enter: "add", "show", "edit, "delete", "complete" : ')
     user_input = user_input.strip()
+
+
     #     add
     if user_input.startswith("add") or user_input.startswith("new"):
         add_todo = user_input[4:]
-        with open('todo_list.txt', 'a') as file:
-            file.write(add_todo)
-            print(f"Successfully added '{add_todo}' to a list.")
+
+        todo_list = get_list()
+            # print(f"Successfully added '{add_todo}' to a list.")
+        todo_list.append(add_todo + "\n")
 
         with open('date_log.txt', 'a') as file_date:
-            file_date.writelines(f"{str(datetime.datetime.now(pytz.timezone('Europe/Zagreb')))} - added to: {file.name}" + "\n")
+            file_date.writelines(f"{str(datetime.datetime.now(pytz.timezone('Europe/Zagreb')))} - added to: list" + "\n")
+
 
     #     show
     elif user_input.startswith("show"):
-        # print("Files: ")
-        # for i, filename in enumerate(file_list):
-        #     print(f"{i + 1} - {filename}")
-        # file_open = int(input("Select file: ")) - 1
-        #
-        with open('todo_list.txt', 'r') as file:
-            todo_list = file.readlines()
-            for i, item in enumerate(todo_list):
-                item = item.strip("\n")
-                print(f"{i + 1}. {item} ")
+
+        todo_list = get_list()
+        for i, item in enumerate(todo_list):
+            item = item.strip("\n")
+            print(f"{i + 1}. {item} ")
+
 
     #   edit
     elif user_input.startswith("edit"):
-
-
         try:
-            with open('todo_list.txt', 'r') as file:
-                todo_list = file.readlines()
-
+            todo_list = get_list()
             num_edit = int(user_input[5:]) - 1
             old_todo = todo_list[num_edit]
             confirm = input(f"Are you sure you want to edit {old_todo.strip()} ? (y/n): ")
@@ -53,11 +62,11 @@ while True:
                 with open('todo_list.txt', 'w') as file:
                     file.writelines(todo_list)
                     print(f"{file.name} has been edited. Item number {num_edit+1}: {todo_list[num_edit]}")
+
                 with open('date_log.txt', 'a') as file_date:
                     file_date.writelines(f"{str(datetime.datetime.now())} - edited: {file.name}"+"\n")
             else:
                 print('Nothing edited.')
-
         except ValueError:
             print("Input not valid.")
 
@@ -65,12 +74,11 @@ while True:
     #     delete
     elif user_input.startswith("delete"):
         # 1. iterates and shows items in a file imported as -> todo_list
-        with open('todo_list.txt', 'r') as file:
-            todo_list = file.readlines()
-            for i, item in enumerate(todo_list):
-                item = item.strip("\n")
-                row_item = (f"{i + 1}. {item} ")
-                print(row_item)
+        get_list()
+        for i, item in enumerate(todo_list):
+            item = item.strip("\n")
+            row_item = (f"{i + 1}. {item} ")
+            print(row_item)
         # 2. manipulating imported
         num_del = int(input("Enter number to delete: ")) - 1
         deleted_todo = todo_list.pop(num_del)
@@ -88,11 +96,10 @@ while True:
     #     complete
     elif user_input.startswith("complete"):
 
-        with open('todo_list.txt', 'r') as file:
-            todo_list = file.readlines()
-            for i, item in enumerate(todo_list):
-                item = item.strip("\n")
-                print(f"{i + 1}. {item} ")
+        todo_list = get_list()
+        for i, item in enumerate(todo_list):
+            item = item.strip("\n")
+            print(f"{i + 1}. {item} ")
 
         completed_todo = int(user_input[9:]) -1
         new_todo = todo_list.pop(completed_todo)
@@ -108,9 +115,12 @@ while True:
         message = f"Todo {new_todo.strip()} was added to a list."
         print(message)
 
+
     #   exit
     elif 'exit' in user_input:
         break
 
+
+    #   rerun
     else:
         print('Type: add, show, edit, delete, complete or exit.')
