093470a3a5818c40e0d897a09479363f57efe965
Daniels-acc <gluhakdaniel@gmail.com>
2023-05-08 23:27:30 +0200

    Added isinstance() which checks if a passed arguments are valid (int or float)

--- my_bonuscode/converter.py
+++ my_bonuscode/converter.py
@@ -5,6 +5,7 @@ feet_inch = input("Enter feet and inches: ")
 
 def convert(user_arg):
     index_error_message = "Please provide two arguments: feet and inches."
+    value_error_message = "Please enter numbers only."
 
     try:
         parts = user_arg.split(" ")
@@ -15,9 +16,19 @@ def convert(user_arg):
 
         meters = (feet*0.3048) + (inch*0.0254)
         meters = meters.__round__(3)
-        return f"{feet} feet and {inch} inches = {meters} meters"
+        return meters
     except IndexError:
         return index_error_message
+    except ValueError:
+        return value_error_message
 
 
-print(convert(feet_inch))
+result: float | str = convert(feet_inch)
+#
+if not isinstance(result, (int, float)):
+    print("Wrong arguments.")
+    exit()
+elif result < 1.25:
+    print("Child height is not enough for the pool slider.")
+else:
+    print("Child can use the slide.")
