2856af5bd3381109ecc683cc707046775bf479a0
Daniels-acc <gluhakdaniel@gmail.com>
2023-05-08 22:52:08 +0200

    Surround with try except IndexError

--- my_bonuscode/converter.py
+++ my_bonuscode/converter.py
@@ -4,16 +4,20 @@ feet_inch = input("Enter feet and inches: ")
 
 
 def convert(user_arg):
+    index_error_message = "Please provide two arguments: feet and inches."
 
-    parts = user_arg.split(" ")
-    parts = [string.replace(",", ".") for string in parts]
+    try:
+        parts = user_arg.split(" ")
+        parts = [string.replace(",", ".") for string in parts]
 
-    feet = float(parts[0])
-    inch = float(parts[1])
+        feet = float(parts[0])
+        inch = float(parts[1])
 
-    meters = (feet*0.3048) + (inch*0.0254)
-    meters = meters.__round__(3)
-    return f"{feet} feet and {inch} inches = {meters} meters"
+        meters = (feet*0.3048) + (inch*0.0254)
+        meters = meters.__round__(3)
+        return f"{feet} feet and {inch} inches = {meters} meters"
+    except IndexError:
+        return index_error_message
 
 
 print(convert(feet_inch))
