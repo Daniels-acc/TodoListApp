7a49de751d6ad7b5f181eb7df5d57257d1576945
daniel.gluhak <gluhakdaniel@gmail.com>
2023-05-17 13:11:04 +0200

    Imported webbrowser module and accepting user input for searching google. Made an executable file

---
+++ experiments/experiment_15_webbrowser.py
@@ -0,0 +1,8 @@
+import shutil
+import webbrowser
+
+user_search = input("Search: ")
+
+# webbrowser.open(f"https://www.google.com/search?q={user_search}")
+webbrowser.open_new_tab(f"https://www.google.com/search?q={user_search}")
+
